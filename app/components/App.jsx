dcec216177215dbf710d94606fe2aefb3b71ad2f
marineco <marinecolonge@gmail.com>
2017-01-04 15:13:38 +0100

    ajout handleClick sur bouton Start + compteur fonctionnel ds console

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 
+  	let startTime;
+  	let intervalID;
+  	let time = 1500;
+  	const count = 1;
+
 export default class App extends React.Component {
+	
+  start() {
+  	startTime = time;
+  	intervalID = setInterval(() => {
+
+    	const hour = Math.floor(time/3600);
+    	const minute = Math.floor((time % 3600)/60);
+    	const second = time % 3600 % 60;
+    	console.log(second);
+
+  		const timer = time--;
+
+  		console.log(timer);
+
+  	}, 1000);
+  }
+
   render() {
     return (
       <div id="content">
@@ -11,7 +33,7 @@ export default class App extends React.Component {
       	</div>
 
       	<div id="buttons">
-      		<button id="btnStart">Start</button>
+      		<button id="btnStart" onClick={this.start}>Start</button>
       		<button id="btnStop">Stop</button>
       		<button id="btnReset">Reset</button>
       	</div>
